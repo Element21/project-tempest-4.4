f93efc82b3ad27bf6782878b3d4489fcde53748a
Tashar02 <tashfinshakeerrhythm@gmail.com>
2021-05-18 16:27:18 +0600

    Revert "qcacld-3.0: Do not allow any wakelocks to be held"
    This reverts commit 7e8d9d0ecf5aa653c3bc7b59e123037d7241ff18.

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
@@ -259,12 +259,6 @@ QDF_STATUS qdf_wake_lock_create(qdf_wake_lock_t *lock, const char *name)
 {
 	return QDF_STATUS_SUCCESS;
 }
-#else
-QDF_STATUS qdf_wake_lock_create(qdf_wake_lock_t *lock, const char *name)
-{
-	return QDF_STATUS_SUCCESS;
-}
-#endif
 qdf_export_symbol(qdf_wake_lock_create);
 
 /**
@@ -295,12 +289,6 @@ QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
 {
 	return QDF_STATUS_SUCCESS;
 }
-#else /* LINUX_VERSION_CODE */
-QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
-{
-	return QDF_STATUS_SUCCESS;
-}
-#endif /* LINUX_VERSION_CODE */
 qdf_export_symbol(qdf_wake_lock_timeout_acquire);
 
 /**
@@ -330,12 +318,6 @@ QDF_STATUS qdf_wake_lock_destroy(qdf_wake_lock_t *lock)
 {
 	return QDF_STATUS_SUCCESS;
 }
-#else
-QDF_STATUS qdf_wake_lock_destroy(qdf_wake_lock_t *lock)
-{
-	return QDF_STATUS_SUCCESS;
-}
-#endif
 qdf_export_symbol(qdf_wake_lock_destroy);
 
 #ifdef CONFIG_MCL
